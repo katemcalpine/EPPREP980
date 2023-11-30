248bd0c72c2f1958ac73f6526a0706466629c13c
katemcalpine <49174249+katemcalpine@users.noreply.github.com>
2023-11-30 15:55:41 +1100

    Create AssigningValues.py

---
+++ Day1/Examples1/AssigningValues.py
@@ -0,0 +1,17 @@
+#--------------------------------------------------------------------
+# Title: AssigningValues.py
+# Author: Kate McAlpine
+# Email: kate.mcalpine@newcastle.edu.au
+# Created: 15th November, 2023
+# Found in: Day 1 Lecture Slides
+#--------------------------------------------------------------------
+
+apple = 5
+
+a4 = 10
+
+SeafoodLaksa = 30
+
+name = 'Gerald'
+
+change = 3.5

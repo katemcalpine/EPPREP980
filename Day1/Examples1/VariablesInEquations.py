477735a6e557d08e67bbe74affbea1032277d53c
katemcalpine <49174249+katemcalpine@users.noreply.github.com>
2023-11-30 16:05:40 +1100

    Create VariablesInEquations.py

---
+++ Day1/Examples1/VariablesInEquations.py
@@ -0,0 +1,12 @@
+#--------------------------------------------------------------------
+# Title: VariablesInEquations.py
+# Author: Kate McAlpine
+# Email: kate.mcalpine@newcastle.edu.au
+# Created: 15th November, 2023
+# Found in: Day 1 Lecture Slides
+#--------------------------------------------------------------------
+
+apple = 5
+basket = 10
+
+total = apple + basket

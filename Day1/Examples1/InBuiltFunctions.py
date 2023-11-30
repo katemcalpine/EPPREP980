12ee0c190768ab7a3973b41b037b0d170531ba38
katemcalpine <49174249+katemcalpine@users.noreply.github.com>
2023-11-30 16:27:26 +1100

    Create InBuiltFunctions.py

---
+++ Day1/Examples1/InBuiltFunctions.py
@@ -0,0 +1,18 @@
+#--------------------------------------------------------------------
+# Title: InBuiltFunctions.py
+# Author: Kate McAlpine
+# Email: kate.mcalpine@newcastle.edu.au
+# Created: 15th November, 2023
+# Found in: Day 1 Lecture Slides
+#--------------------------------------------------------------------
+
+# a = 5
+# b = 10
+
+print(a*b)
+
+input("Name: ")
+
+round(1.5)
+
+int(1.5)

9736308fdb32f2fd34fd2c2b44014c22c8512666
katemcalpine <49174249+katemcalpine@users.noreply.github.com>
2023-12-05 11:25:54 +1100

    Create ConsoleIODirect.py

---
+++ Day1/Examples1/ConsoleIODirect.py
@@ -0,0 +1,17 @@
+#--------------------------------------------------------------------
+# Title: ConsoleIOVariables.py
+# Author: Kate McAlpine
+# Email: kate.mcalpine@newcastle.edu.au
+# Created: 15th November, 2023
+# Found in: Day 1 Lecture Slides
+#--------------------------------------------------------------------
+
+a = 5
+b = 10
+print(a*b)
+
+print(input("Name: "))
+
+print(round(1.5))
+
+print(int(1.5))

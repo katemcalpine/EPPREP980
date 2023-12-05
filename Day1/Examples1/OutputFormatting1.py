854e169260b0d570317d389ee9fe09348be59e33
katemcalpine <49174249+katemcalpine@users.noreply.github.com>
2023-12-05 11:28:25 +1100

    Create OutputFormatting1.py

---
+++ Day1/Examples1/OutputFormatting1.py
@@ -0,0 +1,12 @@
+#--------------------------------------------------------------------
+# Title: OutputFormatting1.py
+# Author: Kate McAlpine
+# Email: kate.mcalpine@newcastle.edu.au
+# Created: 15th November, 2023
+# Found in: Day 1 Lecture Slides
+#--------------------------------------------------------------------
+
+loaf = 4.50
+muffin = 2.50
+total = loaf + muffin
+print('Total: $', total, '. Thank you.')

b685c7da6e42110d755160b536851e57a70cccd7
katemcalpine <49174249+katemcalpine@users.noreply.github.com>
2023-12-05 11:22:28 +1100

    Create ConsoleIOVariables.py

---
+++ Day1/Examples1/ConsoleIOVariables.py
@@ -0,0 +1,20 @@
+#--------------------------------------------------------------------
+# Title: InBuiltFunctions.py
+# Author: Kate McAlpine
+# Email: kate.mcalpine@newcastle.edu.au
+# Created: 15th November, 2023
+# Found in: Day 1 Lecture Slides
+#--------------------------------------------------------------------
+
+a = 5
+b = 10
+print(a*b)
+
+name = input("Name: ")
+print(name)
+
+number = round(1.5)
+print(number)
+
+number = int(1.5)
+print(number)

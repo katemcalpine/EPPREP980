4ba8aedab842250fd063ec56e786b1129ad9307c
katemcalpine <49174249+katemcalpine@users.noreply.github.com>
2024-02-12 19:54:46 +1100

    Create WhileLoop.py

---
+++ Day2/Examples2/WhileLoop.py
@@ -0,0 +1,12 @@
+#--------------------------------------------------------------------
+# Title: WhileLoop.py
+# Author: Kate McAlpine
+# Email: kate.mcalpine@newcastle.edu.au
+# Created: 20th November, 2023
+# Found in: Day 2 Lecture Slides
+#--------------------------------------------------------------------
+
+a = 5
+while (a <= 5):
+	a = a + 1
+print(a)

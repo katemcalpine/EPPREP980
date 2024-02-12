753fae39569fe426bac6cbd8c82f991ab9519f68
katemcalpine <49174249+katemcalpine@users.noreply.github.com>
2024-02-12 19:50:20 +1100

    Create ForLoop.py

---
+++ Day2/Examples2/ForLoop.py
@@ -0,0 +1,12 @@
+#--------------------------------------------------------------------
+# Title: ForLoop.py
+# Author: Kate McAlpine
+# Email: kate.mcalpine@newcastle.edu.au
+# Created: 20th November, 2023
+# Found in: Day 2 Lecture Slides
+#--------------------------------------------------------------------
+
+a = 5
+for counter in range(0, 5)
+	a = a + 1
+print(a)

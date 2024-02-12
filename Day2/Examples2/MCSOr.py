bc88830c330fd32278dc0a48a769a2ed0725f553
katemcalpine <49174249+katemcalpine@users.noreply.github.com>
2024-02-12 19:48:57 +1100

    Create MCSOr.py

---
+++ Day2/Examples2/MCSOr.py
@@ -0,0 +1,17 @@
+#--------------------------------------------------------------------
+# Title: MCSOr.py
+# Author: Kate McAlpine
+# Email: kate.mcalpine@newcastle.edu.au
+# Created: 20th November, 2023
+# Found in: Day 2 Lecture Slides
+#--------------------------------------------------------------------
+
+a = 5
+b = 10
+if (a == 5) or (a == 10):
+	b = 20
+elif (a == 10):
+	b = 30
+else:
+	b = 40
+print(b)

b8dda77c5c3593d02f286f88028984d899c66d80
katemcalpine <49174249+katemcalpine@users.noreply.github.com>
2024-02-12 21:04:12 +1100

    Create ScatterPlot2.py

---
+++ Day3/Examples3/ScatterPlot2.py
@@ -0,0 +1,26 @@
+#--------------------------------------------------------------------
+# Title: ScatterPlot2.py
+# Author: Kate McAlpine
+# Email: kate.mcalpine@newcastle.edu.au
+# Created: 25th November, 2023
+# Found in: Day 3 Lecture Slides
+#--------------------------------------------------------------------
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+np.random.seed(1)
+
+x1 = np.array([5, 7, 8, 3, 4, 7, 9, 10, 1, 2])
+y1 = np.array([14, 15, 23, 4, 10, 18, 12, 12, 16, 5])
+x2 = np.random.randint(0,11,10)
+y2 = np.random.randint(10,21,10)
+
+plt.scatter(x1, y1)
+plt.scatter(x2, y2)
+plt.title('Scatter Plot')
+plt.ylabel('Y Coordinate')
+plt.xlabel('X Coordinate')
+plt.legend(['Location 1’, ‘Location 2’])
+plt.grid(False)
+plt.show()

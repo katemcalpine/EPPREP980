9b181d931f760d5123ab337dfb3751c28142c063
katemcalpine <49174249+katemcalpine@users.noreply.github.com>
2024-02-12 19:05:47 +1100

    Create IfStatement.py

---
+++ Day2/Examples1/IfStatement.py
@@ -0,0 +1,13 @@
+#--------------------------------------------------------------------
+# Title: IfStatement.py
+# Author: Kate McAlpine
+# Email: kate.mcalpine@newcastle.edu.au
+# Created: 20th November, 2023
+# Found in: Day 2 Lecture Slides
+#--------------------------------------------------------------------
+
+a = 5
+b = 10
+if (a == 5):
+  b = 20
+print(b)

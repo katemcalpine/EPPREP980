c46f3a729db172bb5fab47f8b6fcd07fe808a433
katemcalpine <49174249+katemcalpine@users.noreply.github.com>
2024-02-12 20:41:40 +1100

    Create RecurringCode.py

---
+++ Day3/Examples3/RecurringCode.py
@@ -0,0 +1,20 @@
+#--------------------------------------------------------------------
+# Title: RecurringCode.py
+# Author: Kate McAlpine
+# Email: kate.mcalpine@newcastle.edu.au
+# Created: 25th November, 2023
+# Found in: Day 3 Lecture Slides
+#--------------------------------------------------------------------
+
+a = 5
+a = a + 10
+print(a)
+a = 13
+a = a + 10
+print(a)
+a = 27
+a = a + 10
+print(a)
+a = 16
+a = a + 10
+print(a)

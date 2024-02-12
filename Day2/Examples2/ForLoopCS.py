7822e011d3c3de7a7f29692cfab6cc756ff19f92
katemcalpine <49174249+katemcalpine@users.noreply.github.com>
2024-02-12 19:51:43 +1100

    Create ForLoopCS.py

---
+++ Day2/Examples2/ForLoopCS.py
@@ -0,0 +1,20 @@
+#--------------------------------------------------------------------
+# Title: ForLoopCS.py
+# Author: Kate McAlpine
+# Email: kate.mcalpine@newcastle.edu.au
+# Created: 20th November, 2023
+# Found in: Day 2 Lecture Slides
+#--------------------------------------------------------------------
+
+a = 5
+b = 10
+for i in range(0, 10, 2)
+	if (a == 5):
+		b = 20
+	elif (a == 10):
+		b = 30
+	else:
+		b = 40
+	print(b)
+	a = a + 1
+print(a, b)

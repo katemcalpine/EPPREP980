ae09199b83debac59558ed8c5893fca6f2da5487
katemcalpine <49174249+katemcalpine@users.noreply.github.com>
2024-02-14 22:14:09 +1100

    Create Creating2DArray.py

---
+++ Day4/Examples4/Creating2DArray.py
@@ -0,0 +1,13 @@
+#--------------------------------------------------------------------
+# Title: Creating2DArray.py
+# Author: Kate McAlpine
+# Email: kate.mcalpine@newcastle.edu.au
+# Created: 30th November, 2023
+# Found in: Day 4 Lecture Slides
+#--------------------------------------------------------------------
+
+import numpy as np
+
+a = np.array([[12, 37, 24], [12, 42, 5], [36, 7, 13]])
+
+print(a)

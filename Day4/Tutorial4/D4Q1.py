8245d5ddbc4671c53d2f329728caffd02826cb4d
katemcalpine <49174249+katemcalpine@users.noreply.github.com>
2024-02-14 21:59:38 +1100

    Create D4Q1.py

---
+++ Day4/Tutorial4/D4Q1.py
@@ -0,0 +1,16 @@
+#--------------------------------------------------------------------
+# Title: D4Q1.py
+# Author: Kate McAlpine
+# Email: kate.mcalpine@newcastle.edu.au
+# Created: 14th February, 2024
+# Found in: Day 4 Tutorial Slides
+#--------------------------------------------------------------------
+
+import numpy as np
+
+a = np.zeros((5,5))
+a[:,0] = 1
+a[:,2] = 1
+a[:,4] = 1
+
+print(a)

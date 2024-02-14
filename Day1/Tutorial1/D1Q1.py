1d5ab0bedf3a9960e854b9ee859bfcc152ce94c9
katemcalpine <49174249+katemcalpine@users.noreply.github.com>
2024-02-14 21:45:55 +1100

    Create D1Q1.py

---
+++ Day1/Tutorial1/D1Q1.py
@@ -0,0 +1,9 @@
+#--------------------------------------------------------------------
+# Title: D1Q1.py
+# Author: Kate McAlpine
+# Email: kate.mcalpine@newcastle.edu.au
+# Created: 14th February, 2024
+# Found in: Day 1 Tutorial Slides
+#--------------------------------------------------------------------
+
+print('your name')

088daef5506395a53784de437524b07841dfab3d
katemcalpine <49174249+katemcalpine@users.noreply.github.com>
2024-02-16 17:06:03 +1100

    Create D5Q1.py

---
+++ Day5/Tutorial5/D5Q1.py
@@ -0,0 +1,11 @@
+#--------------------------------------------------------------------
+# Title: D5Q1.py
+# Author: Kate McAlpine
+# Email: kate.mcalpine@newcastle.edu.au
+# Created: 16th February, 2024
+# Found in: Day 5 Lecture Slides
+#--------------------------------------------------------------------
+
+import matplotlib.pyplot as plt
+
+a = plt.imread('SunflowerFilter.png')

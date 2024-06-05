8bc90ae6c0adc26253e2613a77947a28ef1fc7d0
katemcalpine <49174249+katemcalpine@users.noreply.github.com>
2024-06-05 19:11:05 +1000

    Update D2Q5.py

--- Day2/Tutorial2/D2Q5.py
+++ Day2/Tutorial2/D2Q5.py
@@ -2,25 +2,17 @@
 # Title: D2Q5.py
 # Author: Kate McAlpine
 # Email: kate.mcalpine@newcastle.edu.au
-# Created: 14th February, 2024
+# Created: 5th June, 2024
 # Found in: Day 2 Tutorial Slides
 #--------------------------------------------------------------------
 
-import numpy as np
+x = 0
+x1 = 1          # x-1
+x2 = 0          # x-2
+print(x)        # Print initial value
 
-x = np.array([0,1,0,0,0,0,0,0,0])
-
-# Long way:
-x[2] = x[0] + x[1]
-x[3] = x[1] + x[2]
-x[4] = x[2] + x[3]
-x[5] = x[3] + x[4]
-x[6] = x[4] + x[5]
-x[7] = x[5] + x[6]
-x[8] = x[6] + x[7]
-print(x)
-
-# Quicker way:
-for index in range(2, len(x)):
-        x[index] = x[index-1] + x[index-2]
-print(x)
+for i in range(0, 8):
+        x = x1 + x2
+        print(x)
+        x1 = x2         # Shift values
+        x2 = x          # Shift values

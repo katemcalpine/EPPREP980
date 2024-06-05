1c1352b0aa9ad32e7dbaf6ceff10d070b2512baa
katemcalpine <49174249+katemcalpine@users.noreply.github.com>
2024-06-05 17:28:07 +1000

    Add files via upload

---
+++ Day2/Examples2/FunctionWithoutReturn1.py
@@ -0,0 +1,23 @@
+#--------------------------------------------------------------------
+# Title: FunctionWithoutReturn1.py
+# Author: Kate McAlpine
+# Email: kate.mcalpine@newcastle.edu.au
+# Created: 25th November, 2023
+# Found in: Day 3 Lecture Slides
+#--------------------------------------------------------------------
+
+def printResult(a):
+	print(a)
+
+a = 5
+a = a + 10
+printResult(a)
+a = 13
+a = a + 10
+printResult(a)
+a = 27
+a = a + 10
+printResult(a)
+a = 16
+a = a + 10
+print(a)
---
+++ Day2/Examples2/FunctionWithoutReturn3.py
@@ -0,0 +1,16 @@
+#--------------------------------------------------------------------
+# Title: FunctionWithoutReturn3.py
+# Author: Kate McAlpine
+# Email: kate.mcalpine@newcastle.edu.au
+# Created: 25th November, 2023
+# Found in: Day 3 Lecture Slides
+#--------------------------------------------------------------------
+
+def calculation(a):
+	a = a + 10
+	print(a)
+
+calculation(5)
+calculation(13) 
+calculation(27) 
+calculation(16)
